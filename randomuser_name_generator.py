9e11ef0fa14f2806ac58b8e25023c6a7b6d2b1ab
Aakil Ahamed <aakilahmd425@gmail.com>
2023-11-03 22:58:10 +0530

    update code

--- randomuser_name_generator.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def generate_username(length=8):
-
-    username = ''
-    for i in range(length):
-        username += random.choice(string.ascii_lowercase)
-    return username
